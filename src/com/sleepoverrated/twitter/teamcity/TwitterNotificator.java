841bda43a99f85345d8d0bdf8fa8204ee57ab7d9
Scott Cowan <info@scottcowan.org>
2009-03-26 16:07:37 +0000

    first working version, account is configurable thru the My Tools and Settings

--- src/com/sleepoverrated/twitter/teamcity/TwitterNotificator.java
+++ src/com/sleepoverrated/twitter/teamcity/TwitterNotificator.java
@@ -17,56 +17,55 @@ import java.util.ArrayList;
 import java.util.Set;
 
 /**
- * This teamcity plugins is configured as a new notificator. The notifications are text
- * messages, which are sended to the text to speech engine of the Nabaztag.
- * For each teamcity user it is possible to configure his Nabaztag settings and notification events. In this
- * way multiple Nabaztags could be accessed for different projects or developers.
- * The configuration could be found in the same place, where mail or ide notifications are configured.
+ * This teamcity plugin lets you tweet to a twitter account per user based on
+ * your notification settings. The only thing you can currently configure is the
+ * twitter account being used in the 'My Settings and Tools' section.
  *
- * User: Simon Tiffert
- * Copyright: Agimatec GmbH 2008 
+ * Scott Cowan
  */
 public class TwitterNotificator implements Notificator {
 
     private static final String TYPE = "twitterNotifier";
     private static final String TYPE_NAME = "Twitter Notifier";
-    private static final String TWITTER_USER = "Twitter Id";
-    private static final String TWITTER_PASS = "Twitter Password";
+    private static final String TWITTER_USER_ID = "Twitter Id";
+    private static final String TWITTER_PASSWORD = "Twitter Password";
 
+    private static final PropertyKey USER_ID = new NotificatorPropertyKey(TYPE, TWITTER_USER_ID);
+    private static final PropertyKey PASSWORD = new NotificatorPropertyKey(TYPE, TWITTER_PASSWORD);
 
     public TwitterNotificator(NotificatorRegistry notificatorRegistry) throws IOException {
         ArrayList<UserPropertyInfo> userProps = new ArrayList<UserPropertyInfo>();
-        userProps.add(new UserPropertyInfo(TWITTER_USER, "Twitter Username"));
-        userProps.add(new UserPropertyInfo(TWITTER_PASS, "Twitter Password"));
+        userProps.add(new UserPropertyInfo(TWITTER_USER_ID, "Twitter Username"));
+        userProps.add(new UserPropertyInfo(TWITTER_PASSWORD, "Twitter Password"));
         notificatorRegistry.register(this, userProps);
     }
 
     public void notifyBuildStarted(SRunningBuild sRunningBuild, Set<SUser> sUsers) {
-        doNotifications("Build " + sRunningBuild.getFullName() + " started.",sUsers);
+        doNotifications(sRunningBuild.getFullName() + " started.",sUsers);
     }
 
     public void notifyBuildSuccessful(SRunningBuild sRunningBuild, Set<SUser> sUsers) {
-        doNotifications("Build " + sRunningBuild.getFullName() + " successfull.",sUsers);
+        doNotifications(sRunningBuild.getFullName() + " successfull.",sUsers);
     }
 
     public void notifyBuildFailed(SRunningBuild sRunningBuild, Set<SUser> sUsers) {
-        doNotifications("Build " + sRunningBuild.getFullName() + " failed.",sUsers);
+        doNotifications(sRunningBuild.getFullName() + " failed.",sUsers);
     }
 
     public void notifyLabelingFailed(Build build, VcsRoot vcsRoot, Throwable throwable, Set<SUser> sUsers) {
-        doNotifications("Labeling of build " + build.getFullName() + " failed.",sUsers);
+        doNotifications(build.getFullName() + " Labelling failed.",sUsers);
     }
 
     public void notifyBuildFailing(SRunningBuild sRunningBuild, Set<SUser> sUsers) {
-        doNotifications("Build " + sRunningBuild.getFullName() + " is failing.",sUsers);
+        doNotifications(sRunningBuild.getFullName() + " is failing.",sUsers);
     }
 
     public void notifyBuildProbablyHanging(SRunningBuild sRunningBuild, Set<SUser> sUsers) {
-        doNotifications("Build " + sRunningBuild.getFullName() + " is probably hanging.",sUsers);
+        doNotifications(sRunningBuild.getFullName() + " is probably hanging.",sUsers);
     }
 
     public void notifyResponsibleChanged(SBuildType sBuildType, Set<SUser> sUsers) {
-        doNotifications("Responsibility of build " + sBuildType.getFullName() + " changed.",sUsers);
+        doNotifications(sBuildType.getFullName() + " Responsibility changed.",sUsers);
     }
 
     public String getNotificatorType() {
@@ -77,9 +76,9 @@ public class TwitterNotificator implements Notificator {
         return TYPE_NAME;
     }
 
-    public void doNotifications(String message, Set<SUser> sUsers) {
+    public void doNotifications(String message, Set<SUser> sUsers) {     
         for(SUser user : sUsers) {
-            Api api = Api.builder().username(user.getProperties().get(TWITTER_USER)).password(user.getProperties().get(TWITTER_PASS)).build();
+            Api api = Api.builder().username(user.getPropertyValue(USER_ID) ).password(user.getPropertyValue(PASSWORD)).build();
             api.updateStatus(message).build().post();
         }
     }
